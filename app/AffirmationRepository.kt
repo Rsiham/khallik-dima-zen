9185926f62a9de076ca113c4ed52548425c81e9a
Rsiham <128421471+Rsiham@users.noreply.github.com>
2026-06-23 14:28:17 -0400

    Update AffirmationRepository.kt

--- app/AffirmationRepository.kt
+++ app/AffirmationRepository.kt
@@ -1,5 +1,22 @@
+package com.example.khallikdimazen
 
-"بشوية عليك... راك غادي مزيان 💜"
-"نهار جديد، فرصة جديدة"
-"ما خاصكش تكوني كاملة باش تبداي"
-"راه حتى الراحة إنجاز"
+import kotlin.random.Random
+
+class AffirmationRepository {
+
+    private val affirmations = listOf(
+        Affirmation(1, "بشوية عليك... راك غادي مزيان 💜"),
+        Affirmation(2, "نهار جديد فرصة جديدة"),
+        Affirmation(3, "راه حتى الراحة إنجاز"),
+        Affirmation(4, "خدي نفس وكملي خطوة بخطوة"),
+        Affirmation(5, "ما خاصكش تكوني كاملة باش تبداي")
+    )
+
+    fun getAllAffirmations(): List<Affirmation> {
+        return affirmations
+    }
+
+    fun getRandomAffirmation(): Affirmation {
+        return affirmations[Random.nextInt(affirmations.size)]
+    }
+}
