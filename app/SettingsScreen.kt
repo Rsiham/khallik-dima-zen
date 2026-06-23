e07d0298b0a08696ccc41d7c06f6bfa29ebeb657
Rsiham <128421471+Rsiham@users.noreply.github.com>
2026-06-23 14:01:46 -0400

    Update SettingsScreen.kt

--- app/SettingsScreen.kt
+++ app/SettingsScreen.kt
@@ -1,1 +1,22 @@
+package com.example.khallikdimazen
 
+class SettingsScreen {
+
+    var notificationsEnabled: Boolean = true
+
+    var reminderHour: Int = 9
+
+    var darkModeEnabled: Boolean = false
+
+    fun toggleNotifications() {
+        notificationsEnabled = !notificationsEnabled
+    }
+
+    fun toggleDarkMode() {
+        darkModeEnabled = !darkModeEnabled
+    }
+
+    fun updateReminderHour(hour: Int) {
+        reminderHour = hour
+    }
+}
