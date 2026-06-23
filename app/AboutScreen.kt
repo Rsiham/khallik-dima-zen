714e2ce7139b639dbc2f9d34bf3d7ccb53d138b5
Rsiham <128421471+Rsiham@users.noreply.github.com>
2026-06-23 14:26:04 -0400

    Create AboutScreen.kt

---
+++ app/AboutScreen.kt
@@ -0,0 +1,10 @@
+package com.example.khallikdimazen
+
+class AboutScreen {
+
+    fun showAbout() {
+        println("💜 Khallik Dima Zen")
+        println("Application Android de citations positives en Darija.")
+        println("Version 1.0")
+    }
+}
