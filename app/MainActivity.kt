6442f52da636183a4f9c3deaa0dff9306bcaf4e2
Rsiham <128421471+Rsiham@users.noreply.github.com>
2026-06-23 13:49:20 -0400

    Update MainActivity.kt

--- app/MainActivity.kt
+++ app/MainActivity.kt
@@ -1,1 +1,20 @@
 
+package com.example.khallikdimazen
+
+import kotlin.random.Random
+
+fun main() {
+    val affirmations = listOf(
+        "بشوية عليك، راك غادي مزيان 💜",
+        "نهار جديد، فرصة جديدة",
+        "ما خاصكش تكوني كاملة باش تبداي",
+        "خدي نفس وكملي خطوة بخطوة",
+        "راه حتى الراحة إنجاز"
+    )
+
+    val affirmation = affirmations[Random.nextInt(affirmations.size)]
+
+    println("📱 Khallik Dima Zen")
+    println("رسالة اليوم:")
+    println(affirmation)
+}
